3d672e940ec11b2b14dcd45a2ae7da537f8fa74e
faressjebali <fjbeli39@gmail.com>
2024-12-20 17:14:36 +0100

    Update install.py

--- install.py
+++ install.py
@@ -2,9 +2,11 @@ import os
 import shutil
 import platform
 import sys
+import subprocess
 
 def install_script():
-    script_name = "ytb-dwn"
+    script_name = "ytb-dwn.py"  
+    batch_name = "ytb-dwn.bat" 
     script_path = os.path.abspath(script_name)
 
     if platform.system() == "Linux" or platform.system() == "Darwin":  # macOS/Linux
@@ -17,13 +19,18 @@ def install_script():
             print("Permission denied. Try running with sudo.")
     elif platform.system() == "Windows":  # Windows
         dest = input("Enter a directory in your PATH (e.g., C:\\Windows\\System32): ")
+
         if os.path.isdir(dest):
+            batch_content = f'@echo off\npython "{os.path.abspath(script_name)}" %*\n'
+            with open(os.path.join(dest, batch_name), 'w') as batch_file:
+                batch_file.write(batch_content)
+
             shutil.copy(script_path, dest)
-            print(f"Installed {script_name} to {dest}")
+
+            print(f"Installed {script_name} and {batch_name} to {dest}")
         else:
             print("Invalid directory. Make sure it's in your PATH.")
     else:
         print(f"Unsupported OS: {platform.system()}")
 
-if __name__ == "__main__":
-    install_script()
+if _name_ == "_ma
